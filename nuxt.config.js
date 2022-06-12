f6ea5f3c6e8b0fc0e5960cec198e7526fafa0ee8
giovannilaperna <giovannilaperna@gmail.com>
2022-06-12 15:02:21 +0200

    try to fix bug with content module in production

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,25 +1,36 @@
-export default {
+export default async () => {
 
-  target: "static",
-  ssr: "true",
+    const getRoutes = async () => {
+      const { $content } = require('@nuxt/content')
+      const dynamicRoutes = await $content('content').only(['slug']).fetch()
+      return dynamicRoutes.map(myroute => myroute.slug === '/index' ? '/' : '/content/' + myroute.slug)
+    }
 
-  modules: [
+  return {
 
-    ['@nuxtjs/axios', {
-      debug: (process.env.URL) ? false : true,
-      baseURL:  (process.env.URL) ? `https://${process.env.URL}` : 'http://localhost:3000',
-      proxyHeaders: true
-    }],
+    target: "static",
+    ssr: "true",
+  
+    modules: [
+  
+      ['@nuxtjs/axios', {
+        debug: (process.env.URL) ? false : true,
+        baseURL:  (process.env.URL) ? `https://${process.env.URL}` : 'http://localhost:3000',
+        proxyHeaders: true
+      }],
+  
+      '@nuxt/content',
+    ],
+  
+  
+    serverrestMiddleware: process.env.URL ? [] : [
+      '~/rest/index.js',
+    ],
+  
+    build: {
+      quiet: false
+    },
 
-    '@nuxt/content',
-  ],
-
-
-  serverrestMiddleware: process.env.URL ? [] : [
-    '~/rest/index.js',
-  ],
-
-  build: {
-    quiet: false
+    generate: await getRoutes()
   }
 }
