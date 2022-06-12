c6c7f8c03174284ac9d101e4fc4747ba1117f2a4
giovannilaperna <giovannilaperna@gmail.com>
2022-06-12 15:31:43 +0200

    try to fix bug with content module in production

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,75 @@
-export default async () => {
+// export default async () => {
 
-  return {
+//   return {
 
-    target: "static",
-    ssr: "true",
+//     target: "static",
+//     ssr: "true",
   
-    modules: [
+//     modules: [
   
-      ['@nuxtjs/axios', {
-        debug: (process.env.URL) ? false : true,
-        baseURL:  (process.env.URL) ? `https://${process.env.URL}` : 'http://localhost:3000',
-        proxyHeaders: true
-      }],
+//       ['@nuxtjs/axios', {
+//         debug: (process.env.URL) ? false : true,
+//         baseURL:  (process.env.URL) ? `https://${process.env.URL}` : 'http://localhost:3000',
+//         proxyHeaders: true
+//       }],
   
-      '@nuxt/content',
-    ],
+//       '@nuxt/content',
+//     ],
   
   
-    serverrestMiddleware: process.env.URL ? [] : [
-      '~/rest/index.js',
-    ],
+//     serverrestMiddleware: process.env.URL ? [] : [
+//       '~/rest/index.js',
+//     ],
   
-    build: {
-      quiet: false
-    },
+//     build: {
+//       quiet: false
+//     },
 
-    generate: async () => {
-      const { $content } = require('@nuxt/content')
+//     generate: async () => {
+//       const { $content } = require('@nuxt/content')
     
-      const posts = await $content('content')
-        .only(['path'])
-        .fetch()
+//       const posts = await $content('content')
+//         .only(['path'])
+//         .fetch()
     
-      return posts.map(post => post.path === '/index' ? '/' : '/content/' + post.path)
-    }
+//       return posts.map(post => post.path === '/index' ? '/content' : '/content/' + post.path)
+//     }
+//   }
+// }
+
+
+export default {
+
+  target: "static",
+  ssr: "true",
+
+  modules: [
+
+    ['@nuxtjs/axios', {
+      debug: (process.env.URL) ? false : true,
+      baseURL:  (process.env.URL) ? `https://${process.env.URL}` : 'http://localhost:3000',
+      proxyHeaders: true
+    }],
+
+    '@nuxt/content',
+  ],
+
+
+  serverrestMiddleware: process.env.URL ? [] : [
+    '~/rest/index.js',
+  ],
+
+  build: {
+    quiet: false
+  },
+
+  generate: async () => {
+    const { $content } = require('@nuxt/content')
+  
+    return $content('content')
+      .only(['path'])
+      .fetch().then(posts => {
+        return posts.map(post => post.path === '/index' ? '/content' : '/content/' + post.path)
+      })
   }
 }
